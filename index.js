f50fa423d563e22f7450b53006b6c8b5f9cc2014
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-09-21 19:09:44 -0300

    feat: init serverless structure

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", async function (req, res) {
+  res.status(200).json("OK");
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+
+module.exports.handler = serverless(app);
