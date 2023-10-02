e258667cfa90b23290d0ee8bbdea5304e29bbb32
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-10-01 21:10:31 -0300

    chore: use alias import inside use cases

--- src/application/use-cases/add-access-counter.ts
+++ src/application/use-cases/add-access-counter.ts
@@ -1,5 +1,5 @@
-import { AccessCounterInputDomain } from "../../domain/contracts/add-access-counter-input";
-import { DynamoDBCounterService } from "../../infra/providers/dynamodb-counter-service";
+import { AccessCounterInputDomain } from "@domain/contracts/add-access-counter-input";
+import { DynamoDBCounterService } from "@infra/providers/dynamodb-counter-service";
 import { AccessCounterService } from "../services/access-counter";
 
 export class AddAccessCounterUseCase {
--- src/application/use-cases/get-user.ts
+++ src/application/use-cases/get-user.ts
@@ -1,8 +1,8 @@
 import { UserNotFoundError } from "@domain/errors/user-not-found";
-import { GetUserInputDomain } from "../../domain/contracts/get-user-input";
-import { User } from "../../domain/contracts/user";
-import { UserRepository } from "../../domain/repositories/user-repository";
-import { DynamoDBUserRepository } from "../../infra/repositories/dynamodb/user/dynamodb-user-repository";
+import { GetUserInputDomain } from "@domain/contracts/get-user-input";
+import { User } from "@domain/contracts/user";
+import { UserRepository } from "@domain/repositories/user-repository";
+import { DynamoDBUserRepository } from "@infra/repositories/dynamodb/user/dynamodb-user-repository";
 
 export class GetUserUseCase {
   private userRepository: UserRepository;
